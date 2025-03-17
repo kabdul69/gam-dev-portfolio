3b8c0998e74deacf0666a19209c1478389fe9fda
abdul12-dev <reviewologe@gmail.com>
2025-03-17 23:14:31 +0500

    Restore project and add latest changes

--- src/App.js
+++ src/App.js
@@ -2,21 +2,24 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Navbar';
 import Hero from './Hero';
-import Projects from './Projects';
+import Home from './Home';
 import Contact from './Contact';
 import Footer from './Footer';
-import Gamedetails from './Gamedetails';
+import GameDetails from './Gamedetails';
 import './App.css';
 
 function App() {
   return (
-    <Router basename="/gam-dev-portfolio">
+    <Router basename="/gam-dev-portfolio"> {/* Use this if deploying on a sub-path */}
       <div className="App">
         <Navbar />
         <Hero />
         <Routes>
-          <Route path="/" element={<Projects />} />
-          <Route path="/game/:id" element={<Gamedetails />} />
+          {/* Home Page */}
+          <Route path="/" element={<Home />} />
+
+          {/* Game Details Page */}
+          <Route path="/game/:id" element={<GameDetails />} />
         </Routes>
         <Contact />
         <Footer />
