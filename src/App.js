f55faaa4c30fde5abf1eca5b60f5ca2388264f84
abdul12-dev <reviewologe@gmail.com>
2025-03-17 09:02:16 +0500

    Fix homepage for Netlify

--- src/App.js
+++ src/App.js
@@ -2,23 +2,20 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Navbar';
 import Hero from './Hero';
-import Projects from './Projects'; // Keep Projects for the home page
+import Projects from './Projects';
 import Contact from './Contact';
 import Footer from './Footer';
-import Gamedetails from './Gamedetails'; // Add GameDetails component
+import Gamedetails from './Gamedetails';
 import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/gam-dev-portfolio">
       <div className="App">
         <Navbar />
         <Hero />
         <Routes>
-          {/* Home Page (Includes Projects) */}
           <Route path="/" element={<Projects />} />
-
-          {/* Game Details Page */}
           <Route path="/game/:id" element={<Gamedetails />} />
         </Routes>
         <Contact />
